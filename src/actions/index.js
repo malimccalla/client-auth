5905ffc17d6f1f17b08879e86b07687e9461b93a
Mali Michael <malimccalla@gmail.com>
2016-12-14 12:09:06 +0000

    Hook up signinUser

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:3000'
+
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    axios.post(`${API_URL}/signin`, { email, password })
+  }
+}
