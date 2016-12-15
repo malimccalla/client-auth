13f946e428994170781dad7cb5800b1ddb2b456c
Mali Michael <malimccalla@gmail.com>
2016-12-15 12:41:19 +0000

    Adds feature component

---
+++ src/components/feature.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+  render() {
+    return (
+      <div>This is a secret feature</div>
+    );
+  }
+}
+
+export default Feature;
