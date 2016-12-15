76b1492b2b56cbc3d307c4d73fbb1623ba0f890d
Mali Michael <malimccalla@gmail.com>
2016-12-15 11:36:59 +0000

    flesh out signup form

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reduxThunk from 'redux-thunk';
 import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
+import Signup from './components/auth/signup';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -17,7 +18,8 @@ ReactDOM.render(
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <Route path="signin" component={Signin} />
-        <Route path="signout" component={Signout}></Route>
+        <Route path="signout" component={Signout} />
+        <Route path="signup" component={Signup} />
       </Route>
     </Router>
   </Provider>,
